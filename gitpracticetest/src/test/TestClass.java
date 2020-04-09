6de2a664e68f78d5a3fb0c7a41872cf7580d316f
Admin <Admin@DESKTOP-C0CNHNA>
2020-04-09 12:15:38 +0530

    testclass

--- gitpracticetest/src/test/TestClass.java
+++ gitpracticetest/src/test/TestClass.java
@@ -5,7 +5,8 @@ public class TestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //This is Test Class
-//This comment is also added from git.		
+//This comment is also added from git.	
+//Added from eclipse		
 	}
 
 }
